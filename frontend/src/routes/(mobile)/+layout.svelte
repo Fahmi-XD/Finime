c325eaacc9389669555ae1e5d1e9da3f93fa9516
Fahmi XD <fahmixd404@gmail.com>
2025-07-25 02:57:29 +0000

    Fix(frontend): Navigation

--- frontend/src/routes/(mobile)/+layout.svelte
+++ frontend/src/routes/(mobile)/+layout.svelte
@@ -6,6 +6,25 @@
 
 	import Navbar from '$lib/components/complex/Navbar.svelte';
 	import { Toaster } from "svelte-french-toast";
+	import { onMount } from 'svelte';
+	import { beforeNavigate, afterNavigate } from '$app/navigation';
+
+  beforeNavigate(({ from, to }) => {
+      console.log('Before navigation:', from, '->', to);
+  });
+  
+  afterNavigate(({ to }) => {
+      console.log('After navigation:', to);
+  });
+
+  onMount(() => {
+      if (typeof window != "undefined") {
+        if ('scrollRestoration' in history) {
+          history.scrollRestoration = 'manual';
+        }
+      }
+  });
+
 
 	const protectedRoute = ["/settings", "/profile"];
 	const authRoute = ["/auth/login", "/auth/register"];
